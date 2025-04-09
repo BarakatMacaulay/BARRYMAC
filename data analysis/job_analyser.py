b8efe0ddddfcf8dbaeca8af4caaa771808a73f09
Barakat Macaulay <barakatmacaulay@gmail.com>
2025-04-09 22:57:45 +0100

    Initial commit

---
+++ data analysis/job_analyser.py
@@ -0,0 +1,4 @@
+def calculate_salary (base_salary, bonus_rate = .1):
+    return base_salary * (1+ bonus_rate)
+def calculate_bonus (total_salary,base_salary):
+    return (total_salary - base_salary) / base_salary
